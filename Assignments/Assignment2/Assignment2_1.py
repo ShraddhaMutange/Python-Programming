b9e14c9879814bfccb26966d1e24b918277cf64a
ShraddhaMutange <shraddhamutange@gmail.com>
2026-06-15 22:18:01 +0530

    Added Assignment-2

---
+++ Assignments/Assignment2/Assignment2_1.py
@@ -0,0 +1,21 @@
+# Problem : Write a program to Display : 
+#           - Value
+#           - Type
+#           - Memory Address
+#           for a variable using appropriate built-in functions.
+
+No = 11
+
+print("Value of No is : ", No)
+print("Type of No is : ", type(No))
+print("Memory address (ID) of No is : ", id(No))
+
+# -----------------------------------------------------------------------
+# ---------------------------------Output--------------------------------
+# -----------------------------------------------------------------------
+# 
+# Value of No is :  11
+# Type of No is :  <class 'int'>
+# Memory address (ID) of No is :  11756008
+# 
+# -----------------------------------------------------------------------
