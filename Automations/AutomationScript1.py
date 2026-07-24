c6616e591ccebb7d3cc6a223614c992b09607c9e
ShraddhaMutange <shraddhamutange@gmail.com>
2026-07-24 21:50:14 +0530

    Add multiple automation scripts for directory scanning, file checksum calculation, and logging

---
+++ Automations/AutomationScript1.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    DirectoryName = sys.argv[1]
+
+    print(len(sys.argv))
+    print(f"Dicrectory name is : {DirectoryName}")
+
+if (__name__ == "__main__"):
+    main()
---
+++ Automations/AutomationScript3.py
@@ -0,0 +1,29 @@
+# python Authomation.py --h
+# python Authomation.py --u
+# python Authomation.py Marvellous
+
+# python Authomation.py --d
+# python Authomation.py Marvellous Demo
+
+import sys
+
+def main():
+    if (len(sys.argv) == 2):
+
+        if (sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("Help")
+
+        elif (sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Usage")
+
+        else:
+            DirectoryName = sys.argv[1]
+            print("Directory name is : ", DirectoryName)
+
+    else:
+        print("Invalid number of arguments")
+        print("Please use --h or --u for more information!")
+
+
+if (__name__ == "__main__"):
+    main()
